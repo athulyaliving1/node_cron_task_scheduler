95a573aa6c8ae8f7769e152e182008d128e05981
athulyaliving1 <muthukumar@athulyaliving.com>
2023-06-03 12:43:15 +0530

    daily task api

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql');
-
+const nodemailer = require("nodemailer");
+var cron = require('node-cron');
 
 
 var indexRouter = require('./routes/index');
@@ -30,9 +31,9 @@ app.use('/users', usersRouter);
 /** @type {*} */
 var conn = mysql.createConnection({
   host: "162.241.123.158",
-  user: "theatgg6_shg",
-  password: "r3pbWhs8psb5nitZjlpDvg",
-  database: "theatgg6_sal_subscriber102"
+  user: "theatgg6_cms",
+  password: "Health@123",
+  database: "theatgg6_cms"
 });
 
 
@@ -44,6 +45,24 @@ conn.connect((err) => {
   console.log('Connected to MySQL as id ' + conn.threadId);
 });
 
+const contactEmail = nodemailer.createTransport({
+  host: "mail.athulyahomecare.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: "noreply@athulyaseniorcare.com", // generated ethereal user
+    pass: "Athulya@123", // generated ethereal password
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
 
 
 // conn.connect((err) => {
@@ -53,23 +72,193 @@ conn.connect((err) => {
 
 
 
+
+
+cron.schedule('28 12 * * *', () => {
+
+
+const CURRENT_DATE = new Date();
+
+  console.log(CURRENT_DATE);
+
+  // let sql = `SELECT * FROM daily_update WHERE department='IT' AND date LIKE '2023-06-02%'`;
+  
+  let sql = `SELECT * FROM daily_update WHERE department='IT' AND date >= '2023-06-02%'`;
+    
+  console.log(sql);
+  let query = conn.query(sql, (err, result) => {
+
+    if (err) throw err;
+
+    // Pass the fetched data to the HTML template
+    const mailOptions = {
+      to: 'muthukumar@athulyaliving.com',
+      subject: "OTP for registration is:",
+      html: `
+        <html>
+          <head>
+            <style>
+              table {
+                border: 1px solid #333;
+                border-collapse: collapse;
+                width: 100%;
+              }
+              th, td {
+                border: 1px solid #333;
+                padding: 8px;
+                text-align: left;
+              }
+              th {
+                background-color: #f2f2f2;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Daily Report</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Date</th>
+                  <th>Department</th>
+                  <th>Details</th>
+                  <th>Pending</th>
+                </tr>
+              </thead>
+              <tbody>
+                ${result
+                  .map(
+                    row => {
+                      const date = new Date(row.date);
+                      const formattedDate = date.toISOString().slice(0, 10);
+                      return `
+                        <tr>
+                          <td>${row.id}</td>
+                          <td>${row.name}</td>
+                          <td>${formattedDate}</td>
+                          <td>${row.department}</td>
+                          <td>${row.details}</td>
+                          <td>${row.pending}</td>
+                        </tr>
+                      `;
+                    }
+                  )
+                  .join('')}
+              </tbody>
+            </table>
+          </body>
+        </html>
+      `,
+    };
+
+    // Send the email with the HTML template
+    contactEmail.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ status: "ERROR" });
+      } else {
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+        res.send(JSON.stringify({ status: 200, error: null, response: result }));
+      }
+    });
+  });
+});
+
+
+
 app.get("/leads", (req, res) => {
-  let sql = "SELECT * FROM leads";
+  const CURRENT_DATE = new Date();
+
+  console.log(CURRENT_DATE);
+
+  // let sql = `SELECT * FROM daily_update WHERE department='IT' AND date LIKE '2023-06-02%'`;
+  
+  let sql = `SELECT * FROM daily_update WHERE department='IT' AND date >= '${CURRENT_DATE}%'`;
+    
+  console.log(sql);
   let query = conn.query(sql, (err, result) => {
     if (err) throw err;
-    res.send(JSON.stringify({ status: 200, error: null, response: result }));
+
+    // Pass the fetched data to the HTML template
+    const mailOptions = {
+      to: 'muthukumar@athulyaliving.com',
+      subject: "OTP for registration is:",
+      html: `
+        <html>
+          <head>
+            <style>
+              table {
+                border: 1px solid #333;
+                border-collapse: collapse;
+                width: 100%;
+              }
+              th, td {
+                border: 1px solid #333;
+                padding: 8px;
+                text-align: left;
+              }
+              th {
+                background-color: #f2f2f2;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Daily Report</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Date</th>
+                  <th>Department</th>
+                  <th>Details</th>
+                  <th>Pending</th>
+                </tr>
+              </thead>
+              <tbody>
+                ${result
+                  .map(
+                    row => {
+                      const date = new Date(row.date);
+                      const formattedDate = date.toISOString().slice(0, 10);
+                      return `
+                        <tr>
+                          <td>${row.id}</td>
+                          <td>${row.name}</td>
+                          <td>${formattedDate}</td>
+                          <td>${row.department}</td>
+                          <td>${row.details}</td>
+                          <td>${row.pending}</td>
+                        </tr>
+                      `;
+                    }
+                  )
+                  .join('')}
+              </tbody>
+            </table>
+          </body>
+        </html>
+      `,
+    };
+
+    // Send the email with the HTML template
+    contactEmail.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ status: "ERROR" });
+      } else {
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+        res.send(JSON.stringify({ status: 200, error: null, response: result }));
+      }
+    });
   });
 });
 
-
-// show a single record
-app.get("/leads/:id", (req, res) => {
-  let sql = "SELECT * FROM leads WHERE id=" + req.params.id;
-  let query = conn.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send(JSON.stringify({ status: 200, error: null, response: result }));
-  });
-});
 
 
 
@@ -92,6 +281,12 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 
