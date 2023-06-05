6a18dff687f2690a988e25e66ca17c7287d1595e
athulyaliving1 <muthukumar@athulyaliving.com>
2023-06-05 13:22:16 +0530

    check fix

--- app.js
+++ app.js
@@ -293,7 +293,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
