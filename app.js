a21acdd90c5fcaafef302cdde4db8b2da27834ed
athulyaliving1 <muthukumar@athulyaliving.com>
2023-06-21 10:01:03 +0530

    time = time

--- app.js
+++ app.js
@@ -354,7 +354,7 @@ cron.schedule(gmtCronSchedule, (res) => {
 
 
 
-const gmtCronSchedules = '58 09 * * *'; // GMT time
+const gmtCronSchedules = '03 10 * * *'; // GMT time
 console.log(`IST cron schedule:', ${gmtCronSchedules}`);
 
 cron.schedule(gmtCronSchedule, (res) => {
